a09728a546196d4eaab7fce55430ab3e0acef78e
xStephx <bojkovskistefan9@gmail.com>
2024-07-12 15:01:28 +0200

    added funcitionality on email button and email input

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// DOM elements
+const inputEmail = document.getElementById('input-email')
+const emailBtn = document.getElementById('email-btn')
+const iconWrong = document.getElementById('icon-wrong')
+const emailMessageWrong = document.getElementById('email-message-wrong')
+
+// Interacting with email button and email input element
+emailBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = inputEmail.value.trim();
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    if(!email){
+        emailMessageWrong.textContent = `Please enter email!`
+        inputEmail.classList.remove('border-DesaturatedRed')
+        inputEmail.classList.add('border-SoftRed')
+        iconWrong.classList.remove('hidden')
+    } else if(!isValidEmail){
+        emailMessageWrong.textContent = `Please provide a valid email!`
+        inputEmail.classList.remove('border-DesaturatedRed')
+        inputEmail.classList.add('border-SoftRed')
+        iconWrong.classList.remove('hidden')   
+    } else{
+        emailMessageWrong.textContent = `Email sent!`
+        emailMessageWrong.classList.remove('text-SoftRed')
+        inputEmail.classList.add('border-DesaturatedRed')
+        inputEmail.classList.remove('border-SoftRed')
+        emailMessageWrong.classList.add('text-lime-600')
+        iconWrong.classList.add('hidden')
+        inputEmail.value = ``
+    }
+})
